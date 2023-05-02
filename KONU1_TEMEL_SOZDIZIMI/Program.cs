574c8ef8903af386aeba0776ed582edc97623e60
semih şenocak <semih.senocak@karegraf.com>
2023-05-02 21:02:33 +0300

    İlk C# kodumuzu yazdık

--- KONU1_TEMEL_SOZDIZIMI/Program.cs
+++ KONU1_TEMEL_SOZDIZIMI/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Merhaba, bu kurmusuzun ilk kodu");
+            Console.ReadKey();
         }
     }
 }
